10e17be324822a07a1ef9800302995d8ed23dc38
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-08-12 20:58:38 +0300

    String Methods

---
+++ strings.py
@@ -0,0 +1,9 @@
+name = ("gathoni wanjira")
+print (name.upper())
+print(name.find("n"))
+# find method returns the index of the character beibg looked for.
+# If you find a character that is not in the variable it returns -1 as the index.
+print(name.replace("gathoni" , "nonnie"))
+
+print("gathoni" in name)
+# In is used to find specific characters and it returns as a boolean , either false or true.
