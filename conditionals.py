716013a4c0c692d1d9f66b3e7404d8b0a85f3ead
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-08-12 21:21:17 +0300

    Conditionals

---
+++ conditionals.py
@@ -0,0 +1,10 @@
+Temparature = 22
+if Temparature > 35:
+    print("Its hot outside today .")
+    print("Wear light clothes .")
+elif Temparature < 10:
+    print("Its Freezing outside please dress warmly.")
+    print("Also remember to take supplements!.")
+else:
+    print("Today is a good weather day!")
+    print("Lets have a picnic!")
